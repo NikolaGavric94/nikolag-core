cdfbdbd927ccd684bd9f740d38c560014f9e18ba
Nikola Gavrić <nikola.gavric94@gmail.com>
2017-10-06 16:15:29 +0200

    Fixed previous exception in constructor to be child of root class instead of Nikolag\Core\Exception

--- src/exceptions/Exception.php
+++ src/exceptions/Exception.php
@@ -5,7 +5,7 @@ use \Exception as BaseException;
 
 class Exception extends BaseException
 {
-    public function __construct(string $message = null, int $code = 0, Exception $previous = null)
+    public function __construct(string $message = null, int $code = 0, BaseException $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
