96b6dd6a169454193b33eed8042a8315bff7eb95
Nikola Gavrić <nikola.gavric94@gmail.com>
2017-10-06 13:48:51 +0200

    Fixed constants

--- src/utils/Constants.php
+++ src/utils/Constants.php
@@ -5,13 +5,13 @@ namespace Nikolag\Core\Utils;
 class Constants
 {
     //Transaction info
-    const TRANSACTION_NAMESPACE    = 'Nikolag\Square\Models\Transaction';
+    const TRANSACTION_NAMESPACE    = 'Nikolag\Core\Models\Transaction';
     const TRANSACTION_IDENTIFIER    = 'id';
     //Transaction statuses
     const TRANSACTION_STATUS_OPENED = 'PENDING';
     const TRANSACTION_STATUS_PASSED = 'PAID';
     const TRANSACTION_STATUS_FAILED = 'FAILED';
     //Customer info
-    const CUSTOMER_NAMESPACE        = 'Nikolag\Square\Models\Customer';
+    const CUSTOMER_NAMESPACE        = 'Nikolag\Core\Models\Customer';
     const CUSTOMER_IDENTIFIER        = 'id';
 }
