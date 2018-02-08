f14add884a4b3c478dd95327a32b2a3f203a47a8
Vianney Rodriguez <36246650+viangela84@users.noreply.github.com>
2018-02-08 01:01:13 -0500

    Update 2017_06_16_140949_create_nikolag_product_order_table.php

--- src/database/migrations/2017_06_16_140949_create_nikolag_product_order_table.php
+++ src/database/migrations/2017_06_16_140949_create_nikolag_product_order_table.php
@@ -14,15 +14,9 @@ class CreateNikolagProductOrderTable extends Migration
      */
     public function up()
     {
-        Schema::create('nikolag_product_order', function (Blueprint $table) {
-            $table->integer('product_id')->unsigned();
-            $table->integer('order_id')->unsigned();
-            $table->integer('quantity')->unsigned();
-        });
-
         Schema::table('nikolag_product_order', function (Blueprint $table) {
-            $table->foreign('product_id')->references('id')->on('products');
-            $table->foreign('order_id')->references('id')->on('orders');
+            $table->foreign('product_id')->references('id')->on('nikolag_products');
+            $table->foreign('order_id')->references('id')->on('nikolag_orders');
             $table->unique(['product_id', 'order_id']);
         });
     }
